1fe8577b7fb76345b9d28e5b3b1358723f5bac2e
Dangocan <danilogc1999@gmail.com>
2021-11-05 05:21:01 -0300

    Add prize system

--- src/pages/Tela3/index.tsx
+++ src/pages/Tela3/index.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { MainModal, OutlinedButton, ListOrder } from "../../components";
+import { Global } from "../../context/GlobalContext";
+
+const PRIZE_NAMES = [
+  "Pequeno (300ml)",
+  "Média (500ml)",
+  "Grande (700ml)",
+  "Morango",
+  "Banana",
+  "Kiwi",
+  "Granola",
+  "Paçoca",
+  "Leite ninho",
+];
+const PRIZE_TABLE = [3, 5, 7, 3, 1, 2, 2, 1, 2];
 
 function Tela3() {
+  const { contextValues } = useContext(Global);
+
+  const acai = {
+    tamanho: contextValues[2]?.values,
+    sabor: contextValues[1]?.values,
+    personalizacao: contextValues[0]?.values,
+  };
+
+  const prize =
+    PRIZE_TABLE[PRIZE_NAMES.indexOf(acai?.tamanho)] +
+    PRIZE_TABLE[PRIZE_NAMES.indexOf(acai?.sabor)] +
+    PRIZE_TABLE[PRIZE_NAMES.indexOf(acai?.personalizacao)];
+
   return (
     <MainModal title="Escolha seu açai">
-      <ListOrder
-        options={[
-          { name: "Tamanho", values: ["Médio (500ml)"] },
-          { name: "Sabor", values: ["Morango"] },
-          { name: "Tamanho", values: ["Paçoca", "leite ninho"] },
-        ]}
-      />
-      <p style={{ color: "purple" }}>Valor total: R$ 16,00</p>
+      <ListOrder options={contextValues} />
+      <p style={{ color: "purple" }}>Valor total: R$ {prize ? prize : 0},00</p>
       <p style={{ color: "purple" }}>Tempo de preparo: 10min</p>
-      <OutlinedButton title="Refazer pedido" />
+      <OutlinedButton isSubmit={false} title="Refazer pedido" />
     </MainModal>
   );
 }
