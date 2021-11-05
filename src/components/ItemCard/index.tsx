2ba354f92e0cfb170a576567d3de171ac726e931
Dangocan <danilogc1999@gmail.com>
2021-11-05 05:19:56 -0300

    Add style change on cards

--- src/components/ItemCard/index.tsx
+++ src/components/ItemCard/index.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import style from "./style";
 
 type ItemCardProps = {
   name: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
 };
 
-function ItemCard({ name }: ItemCardProps) {
+function ItemCard({ name, setValue, value }: ItemCardProps) {
+  const onClick = () => {
+    setValue(name);
+  };
+
   return (
-    <div style={style.Container}>
-      <p style={style.Text}>{name}</p>
+    <div
+      onClick={onClick}
+      style={value === name ? style.ContainerSelected : style.Container}
+    >
+      <p style={value === name ? style.TextSelected : style.Text}>{name}</p>
     </div>
   );
 }
--- src/components/ItemCard/style.ts
+++ src/components/ItemCard/style.ts
@@ -1,12 +1,26 @@
 const Style = {
   Container: {
+    cursor: "pointer",
     padding: "5px 15px",
     margin: "15px 15px 0 0",
     border: "1px solid gray",
     borderRadius: "3px",
   },
+  ContainerSelected: {
+    cursor: "pointer",
+    padding: "5px 15px",
+    margin: "15px 15px 0 0",
+    border: "1px solid purple",
+    borderRadius: "3px",
+  },
   Text: {
+    cursor: "pointer",
     color: "gray",
+    fontSize: ".8rem",
+  },
+  TextSelected: {
+    cursor: "pointer",
+    color: "purple",
     fontSize: ".8rem",
   },
 };
